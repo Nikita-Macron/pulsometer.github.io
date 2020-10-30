5e1929b1ae82beb93b172f738b1ac40b4a34cf0a
nikita-macron <nikitalinnik22@gmail.com>
2020-10-30 17:43:59 +0200

    Made an error output in the form

--- js/script.js
+++ js/script.js
@@ -57,5 +57,35 @@ $(document).ready(function(){
       });
     });
 
-    
+    // При неправильном заполнении формы выводятся ошибки
+    function validateForms(form){
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          number: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа")
+          },
+          number: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты" 
+          }
+        }
+      });
+    };
+
+    validateForms('#order form');
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
   });
